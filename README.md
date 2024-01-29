6dae7080c6d62e2dca3c2efe579c9ef5770ad42e
yantoz <yantoz@users.noreply.github.com>
2024-01-29 22:29:06 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# krita-arrangeLayers
+# Krita Plugin: Arrange Layers
+
+*The missing "align and distribute layers" functionality!*
+ 
+## Installation
+
+1. Open Tools &rarr; Scripts &rarr; Import Python Plugin from File ...
+2. Select arrangeLayers.zip file and install
+
+Or
+
+1. Open Krita, in Settings &rarr; Manage Resources click on the "Open Resource Folder" button
+2. Open "pykrita" folder
+3. Copy all files from plugin .zip file (arrangeLayers folder and arrangeLayers.desktop) to the "pykrita" folder
+
+Restart Krita.
+
+## How to Enable
+
+1. Enable the plugin "Arrange Layers" in Settings &rarr; Configure Krita &rarr; Python Plugin Manager or Krita &rarr; Preferences... &rarr; Python Plugin Manager (MacOS)
+2. Check "Arrange Layers" docker in Settings &rarr; Dockers
+3. New "Arrange Layers" docker should be present in right part of the screen
+
+## Usage
+
+1. Click on a layer to make it active
+2. Ctrl-click on layers to arrange
+3. Press one of the buttons in docker or select an item in Layers &rarr; Arrange Layers to arrange or distribute the selected layers with the active layer
---
+++ arrangeLayers/arrangeLayers.py
@@ -0,0 +1,283 @@
+import krita
+
+from functools import cmp_to_key
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+def moveBy(layer, dx, dy):
+    dx = round(dx)
+    dy = round(dy)
+    pos = layer.position()
+    layer.move(pos.x()+dx, pos.y()+dy)
+    for child in layer.childNodes():
+        moveBy(child, dx, dy)
+
+def moveTo(layer, x, y):
+    pos = layer.bounds()
+    moveBy(layer, x-pos.x(), y-pos.y())
+    
+def testEnable():
+    
+    enableA = False
+    enableD = False
+    
+    doc = Application.activeDocument()
+    if doc is None:
+        return (enableA, enableD, None, None)
+        
+    topLevelLayers = doc.topLevelNodes()
+    activeLayer = doc.activeNode()
+    
+    selectedLayers = Krita.instance().activeWindow().activeView().selectedNodes()
+    processLayers = []
+        
+    for layer in selectedLayers:
+        if layer in topLevelLayers and layer != activeLayer:
+            processLayers.append(layer)
+            
+    enableA = activeLayer and len(processLayers) > 0
+    enableD = activeLayer and len(processLayers) > 1
+    
+    return (enableA, enableD, activeLayer, processLayers)
+            
+def processArrange(alignH, alignV): # 0: NOP, 1: L/T, 2: C/C, 3: R/B, 4: D/D
+
+    enableA, enableD, activeLayer, processLayers = testEnable()
+            
+    if alignH == 4:
+        if not enableD:
+            return
+        layers = [activeLayer] + processLayers
+        layers = sorted(layers, key=cmp_to_key(lambda item1, item2: item1.bounds().x()-item2.bounds().x()))
+        width = 0
+        for layer in layers:
+            width += layer.bounds().width()
+        left = layers[0].bounds().x()
+        right = left
+        for layer in layers:
+            r = layer.bounds().x() + layer.bounds().width() - 1
+            if r > right:
+                right = r
+        span = right - left + 1
+        gap = (span - width) / len(processLayers)
+        x = layers[0].bounds().x() + layers[0].bounds().width() - 1 + gap + 1 
+        for layer in layers[1:]:
+            moveTo(layer, x, layer.bounds().y())
+            x += layer.bounds().width() - 1 + gap + 1                
+    elif alignV == 4:
+        if not enableD:
+            return
+        layers = [activeLayer] + processLayers
+        layers = sorted(layers, key=cmp_to_key(lambda item1, item2: item1.bounds().y()-item2.bounds().y()))
+        height = 0
+        for layer in layers:
+            height += layer.bounds().height()
+        top = layers[0].bounds().y()
+        bottom = top
+        for layer in layers:
+            b = layer.bounds().y() + layer.bounds().height() - 1
+            if b > bottom:
+                bottom = b
+        span = bottom - top + 1
+        gap = (span - height) / len(processLayers)
+        y = layers[0].bounds().y() + layers[0].bounds().height() - 1 + gap + 1 
+        for layer in layers[1:]:
+            moveTo(layer, layer.bounds().x(), y)
+            y += layer.bounds().height() - 1 + gap + 1                
+    else:
+        if not enableA:
+            return
+        activeBounds = activeLayer.bounds()
+        for layer in processLayers:
+            bounds = layer.bounds()
+            if alignH == 1:
+                moveTo(layer, activeBounds.x(), bounds.y())
+            elif alignH == 2:
+                moveTo(layer, activeBounds.x()+(activeBounds.width()-bounds.width())/2, bounds.y())
+            elif alignH == 3:
+                moveTo(layer, activeBounds.x()+activeBounds.width()-bounds.width(), bounds.y())
+            elif alignV == 1:
+                moveTo(layer, bounds.x(), activeBounds.y())
+            elif alignV == 2:
+                moveTo(layer, bounds.x(), activeBounds.y()+(activeBounds.height()-bounds.height())/2)
+            elif alignV == 3:
+                moveTo(layer, bounds.x(), activeBounds.y()+activeBounds.height()-bounds.height())
+            
+    Application.activeDocument().refreshProjection()
+
+def e_alignLeft():
+    processArrange(1, 0)
+    
+def e_alignCenter():
+    processArrange(2, 0)
+
+def e_alignRight():
+    processArrange(3, 0)
+    
+def e_alignTop():
+    processArrange(0, 1)
+    
+def e_alignMiddle():
+    processArrange(0, 2)
+
+def e_alignBottom():
+    processArrange(0, 3)
+    
+def e_distributeH():
+    processArrange(4, 0)
+    
+def e_distributeV():
+    processArrange(0, 4)
+
+class arrangeLayersExtension(krita.Extension):
+ 
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.actions = []
+        self.menu = None
+
+    def setup(self):
+        pass
+ 
+    def createActions(self, window):
+    
+        action = window.createAction("separator0", "", "layer")
+        action.setSeparator(True)
+        
+        action = window.createAction("arrangeLayers", "Arrange Layers", "layer")
+        menu = krita.QtWidgets.QMenu("arrangeLayers", window.qwindow())
+        action.setMenu(menu)
+
+        self.menu = menu
+
+        actions = [
+            ["arrangeLayersLeft", "Align Left Edges", e_alignLeft],
+            ["arrangeLayersCenter", "Align Horizontal Centers", e_alignCenter],
+            ["arrangeLayersRight", "Align Right Edges", e_alignRight],
+            None,
+            ["arrangeLayersTop", "Align Top Edges", e_alignTop],
+            ["arrangeLayersMiddle", "Align Vertical Centers", e_alignMiddle],
+            ["arrangeLayersBottom", "Align Bottom Edges", e_alignBottom],
+            None,
+            ["arrangeLayersDistH", "Distribute Horizontally", e_distributeH],
+            ["arrangeLayersDistV", "Distribute Vertically", e_distributeV],
+        ]
+
+        self.actions = []
+
+        for i, action in enumerate(actions):
+            if action:
+                subaction = window.createAction(action[0], action[1], "layer/arrangeLayers")
+                subaction.triggered.connect(action[2])
+                self.actions.append(subaction)
+            else:
+                subaction = window.createAction("separator{}".format(i+1), "", "layer/arrangeLayers")
+                subaction.setSeparator(True)
+
+        self.setEnable(False, False)
+
+    def setEnable(self, enableA, enableD):
+        if not self.actions:
+            return
+        if self.menu:
+            self.menu.setEnabled(enableA or enableD)
+        try:
+            for i in range(6):
+                self.actions[i].setEnabled(enableA)
+        except:
+            pass
+        try:
+            for i in range(2):
+                self.actions[i+6].setEnabled(enableD)
+        except:
+            pass
+        print([a.isEnabled() for a in self.actions])
+
+class ArrangeLayersDocker(krita.DockWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.buttons = []
+        self.setWindowTitle("Arrange Layers")
+        self.__createDocker()
+
+    def __createDocker(self):
+
+        arrangements = [
+            ["object-align-horizontal-left-calligra", "Align Left Edges", e_alignLeft],
+            ["object-align-horizontal-center-calligra", "Align Horizontal Centers", e_alignCenter],
+            ["object-align-horizontal-right-calligra", "Align Right Edges", e_alignRight],
+            None,
+            ["object-align-vertical-top-calligra", "Align Top Edges", e_alignTop],
+            ["object-align-vertical-center-calligra", "Align Vertical Centers", e_alignMiddle],
+            ["object-align-vertical-bottom-calligra", "Align Bottom Edges", e_alignBottom],
+            None,
+            ["distribute-horizontal", "Distribute Horizontally", e_distributeH],
+            ["distribute-vertical", "Distribute Vertically", e_distributeV],        
+        ]
+    
+        arrangeLayout = QHBoxLayout()
+        arrangeLayout.setSpacing(2)
+        arrangeLayout.setAlignment(krita.Qt.AlignLeft | krita.Qt.AlignTop)
+
+        self.buttons = []
+        
+        for arrangement in arrangements:
+            if arrangement:
+                button = QPushButton()
+                button.setIcon(Krita.instance().icon(arrangement[0]))
+                button.setToolTip(arrangement[1])
+                button.clicked.connect(arrangement[2])
+                arrangeLayout.addWidget(button)
+                self.buttons.append(button)
+            else:
+                arrangeLayout.addWidget(QLabel("  "))
+            
+        widget = QWidget()
+        widget.setLayout(arrangeLayout)
+        widget.resize(0,0)
+    
+        self.setWidget(widget)
+        self.setMinimumHeight(45)
+        self.resize(0,0)
+
+        global docker
+        docker = self
+
+    def setEnable(self, enableA, enableD):
+        if not self.buttons:
+            return
+        try:
+            for i in range(6):
+                self.buttons[i].setEnabled(enableA)
+        except:
+            pass
+        try:
+            for i in range(2):
+                self.buttons[i+6].setEnabled(enableD)
+        except:
+            pass
+
+    def canvasChanged(self, canvas):
+        pass
+
+KI = Krita.instance()
+extension = arrangeLayersExtension(KI)
+docker = None
+
+KI.addExtension(extension)
+KI.addDockWidgetFactory(krita.DockWidgetFactory("Arrange Layers", krita.DockWidgetFactoryBase.DockRight, ArrangeLayersDocker))
+
+def layerChanged():
+    enableA, enableD, _, _ = testEnable()
+    extension.setEnable(enableA, enableD)
+    if docker:
+        docker.setEnable(enableA, enableD)
+
+def windowCreated():
+    qwin = Krita.instance().activeWindow().qwindow()
+    layerBox = qwin.findChild(krita.QtWidgets.QDockWidget, "KisLayerBox")
+    layerList = layerBox.findChild(krita.QtWidgets.QTreeView,"listLayers")
+    layerList.selectionModel().selectionChanged.connect(layerChanged)
+
+KI.notifier().windowCreated.connect(windowCreated)
